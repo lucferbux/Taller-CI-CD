787fb7d6c20d5be6e3610e31ad9cc863ea869fdf
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2023-09-09 22:28:59 +0200

    Add logs

--- api/src/config/connection/connection.ts
+++ api/src/config/connection/connection.ts
@@ -37,6 +37,14 @@ db.on('connected', () => {
 
 db.once('open', () => {
   Logger.info('[MongoDB] connection opened');
+  if(process.env.NODE_ENV === 'test') {
+    Logger.info(`[MongoDB] has the following collections: ${Object.keys(db.collections)}`);
+    const collection  = db.collection("profile");
+    collection.find({}).toArray(function(err, data){
+      Logger.info(`[MongoDB] has the following profile: ${data?.[0]?.name}`);
+  });
+
+  }
 });
 
 db.on('reconnected', () => {
